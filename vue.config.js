f52f6dfaf79475371f735521a6f4a80e9165ee27
B1ANK3 <44206247+B1ANK3@users.noreply.github.com>
2021-05-28 16:55:27 +0200

    new vue config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 
 
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/dnd/'
+        : '/',
     configureWebpack: config => {
         config['devtool'] = 'inline-source-map'
     },
